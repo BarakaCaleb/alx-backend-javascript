8fbfc30d2960748471251bb0357a11591172adc6
BarakaCaleb <calebbaraka79@gmail.com>
2024-07-24 15:07:11 +0300

    changes

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+function handleResponseFromAPI(promise) {
+    return promise
+        .then((response) => {
+            console.log("Got a response from the API");
+            return {
+                status: 200,
+                body: "success",
+            };
+        })
+        .catch((error) => {
+            return new Error();
+        });
+}
