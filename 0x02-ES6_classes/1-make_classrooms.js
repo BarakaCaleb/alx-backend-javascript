d42571ff362f8ee37564b88ed4b8eff3dc0c7d79
BarakaCaleb <calebbaraka79@gmail.com>
2024-07-25 22:23:56 +0300

    import and export

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,12 @@
+import ClassRoom from "./0-classroom";
+
+function initializeRooms() {
+    const object1 = new ClassRoom(19);
+    const object2 = new ClassRoom(20);
+    const object3 = new ClassRoom(24);
+    const list = [object1, object2, object3];
+
+    return(list);
+}
+
+export default initializeRooms;
