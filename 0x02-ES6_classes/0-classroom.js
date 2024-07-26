6f61977018a64a424bfa151abe72518cc6f0e2c9
ysamwel <yvonneatieno2000@gmail.com>
2024-07-26 23:09:23 +0300

    classes

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -2,4 +2,9 @@ export default class ClassRoom {
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
+
+  // Optionally, you can add a getter to access the _maxStudentsSize property
+  get maxStudentsSize() {
+    return this._maxStudentsSize;
+  }
 }
