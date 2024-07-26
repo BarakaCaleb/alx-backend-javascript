5b8900fdcda8d58c4410f86f91029ec3261a2697
BarakaCaleb <calebbaraka79@gmail.com>
2024-07-26 17:32:35 +0300

    Initial commit

--- 0x02-ES6_classes/2-hbtn_course.js
+++
@@ -1,7 +0,0 @@
-export default class HolbertonCourse {
-    constructor(name, length, students) {
-        this._name = name;
-        this._length = length;
-        this._students = students;
-    }
-}
