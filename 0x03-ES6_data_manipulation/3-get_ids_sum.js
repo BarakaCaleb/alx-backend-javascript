b6be18526ba7735646e5c94e3e1eb23548563a69
BarakaCaleb <calebbaraka79@gmail.com>
2024-07-30 02:14:12 +0300

    Task 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,9 @@
-export default function getStudentIdsSum(students) {
-  return list.reduce((sum, student) => sum + student.id, 0);
- }
+export default function getStudentIdsSum(list) {
+    if (!Array.isArray(list)) {
+      return 0;
+    }
+  
+    return list.reduce((accumulator, currentValue) => accumulator + currentValue.id, 0);
+  }
 
+  
