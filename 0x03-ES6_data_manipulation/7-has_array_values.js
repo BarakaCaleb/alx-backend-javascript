5f8c600d5fdc19f81382d3105e3c7816627b0d3b
BarakaCaleb <calebbaraka79@gmail.com>
2024-07-31 02:53:03 +0300

    Task 7

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,11 +1,9 @@
 export default function hasValuesFromArray(set, list) {
-    let verdict = true;
-  
-    list.forEach((x) => {
-      if (!set.has(x)) {
-        verdict = false;
-      }
-    });
-  
-    return verdict;
-  }
+  let verdict = true;
+  list.forEach((x) => {
+    if (!set.has(x)) {
+      verdict = false;
+    }
+  });
+  return verdict;
+}
