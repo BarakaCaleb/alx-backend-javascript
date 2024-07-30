c40389e3b26e685ff7683caec030967382baff96
BarakaCaleb <calebbaraka79@gmail.com>
2024-07-31 02:42:53 +0300

    Task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,23 +1,17 @@
 export default function updateStudentGradeByCity(list, city, newGrade) {
-    return list
-      .filter((obj) => obj.location === city)
-      .map((student) => {
+  return list
+    .filter((obj) => obj.location === city)
+    .map((student) => {
       // Create a copy of the student object
-        const updatedStudent = { ...student };
-  
-        // Update the grade if found in newGrade
-        newGrade.forEach((studentGrade) => {
-          if (studentGrade.studentId === student.id) {
-            updatedStudent.grade = studentGrade.grade;
-          }
-        });
-  
-        // If the grade is not found, set it to 'N/A'
-        if (!Object.prototype.hasOwnProperty.call(updatedStudent, 'grade')) {
-          updatedStudent.grade = 'N/A';
+      const updatedStudent = { ...student };
+      newGrade.forEach((studentGrade) => {
+        if (studentGrade.studentId === student.id) {
+          updatedStudent.grade = studentGrade.grade;
         }
-  
-        return updatedStudent;
       });
-  }
-
+      if (!Object.prototype.hasOwnProperty.call(updatedStudent, 'grade')) {
+        updatedStudent.grade = 'N/A';
+      }
+      return updatedStudent;
+    });
+}
