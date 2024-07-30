4624879929d43aef4776ea75dc1bd9447e9181ea
BarakaCaleb <calebbaraka79@gmail.com>
2024-07-31 02:47:16 +0300

    Task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,15 +1,11 @@
 export default function updateUniqueItems(map) {
-    if (map instanceof Map) {
-      for (const [key, value] of map) {
-        if (value === 1) {
-          map.set(key, 100);
-        }
+  if (map instanceof Map) {
+    for (const [key, value] of map) {
+      if (value === 1) {
+        map.set(key, 100);
       }
-  
-      return map;
     }
-  
-    throw new Error('Cannot process');
+    return map;
   }
-
-  
+  throw new Error('Cannot process');
+}
