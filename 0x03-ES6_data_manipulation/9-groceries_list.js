c662978a5c78e8deccc63a80f3f28cdb1fedc3b9
BarakaCaleb <calebbaraka79@gmail.com>
2024-07-31 02:50:10 +0300

    Task 9

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,17 +1,14 @@
 export default function groceriesList() {
-    const finalMap = new Map();
-    const obj = {
-      Apples: 10,
-      Tomatoes: 10,
-      Pasta: 1,
-      Rice: 1,
-      Banana: 5,
-    };
-      //   get all the keys in the object into an array
-    const list = Array.from(Object.keys(obj));
-  
-    list.map((item) => finalMap.set(item, obj[item]));
-    return finalMap;
-  }
-
-  
+  const finalMap = new Map();
+  const obj = {
+    Apples: 10,
+    Tomatoes: 10,
+    Pasta: 1,
+    Rice: 1,
+    Banana: 5,
+  };
+  //   get all the keys in the object into an array
+  const list = Array.from(Object.keys(obj));
+  list.map((item) => finalMap.set(item, obj[item]));
+  return finalMap;
+}
