4006c1b58ed4cbf9421483e1773ed28b10550438
BarakaCaleb <calebbaraka79@gmail.com>
2024-07-31 03:01:54 +0300

    Task 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,10 +1,6 @@
-export default function getListStudentIds(students) {
-    // Check if the passed argument is an array
-    if (!Array.isArray(students)) {
-        return [];
-    }
-    // Utilise map to extract the ids from the student objects
-    return students.map(student => student.id);
+export default function getListStudentIds(list) {
+  if (Array.isArray(list)) {
+    return list.map((obj) => obj.id);
+  }
+  return [];
 }
-
-
