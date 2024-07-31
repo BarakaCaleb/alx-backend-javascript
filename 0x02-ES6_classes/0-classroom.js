bef89db2ccaddb33affd725d28bc72c3f3d45c62
BarakaCaleb <calebbaraka79@gmail.com>
2024-07-31 03:16:48 +0300

    Task 0

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,7 +1,7 @@
 export default class ClassRoom {
-    constructor(maxStudentsSize) {
-      this._maxStudentsSize = maxStudentsSize;
-    }
+  constructor(maxStudentsSize) {
+  this._maxStudentsSize = maxStudentsSize;
   }
+}
 
   
